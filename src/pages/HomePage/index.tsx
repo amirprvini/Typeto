26a43123f7f9806631f4877a89d9c9c93c76b97b
amirparvini <amirparvini2000@gmail.com>
2024-04-15 12:23:26 +0330

    slider created for home page

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -12,49 +12,26 @@ import PersonalityTypesCard from '../../components/PersonalityTypesCard'
 import { API_URLS } from '../../constants/api.urls'
 import { AXIOS } from '../../config/axios.config'
 import { AppContext } from '../../components/context/store'
+import { UseAllArtistQuery } from '../../components/services/queries/useAllArtistsQuery'
+import { artistType } from '../../components/AddCommentSection'
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import ArtistCard from '../../components/ArtistCard'
 let counter = 1; 
 let typesCounter = 1; 
+
 
 export const HomePage : React.FC = () : JSX.Element =>{
 
     const user = useContext(AppContext);
     const [state,setState] = useState(false);
     const [types,setTypes] = useState<any[]>([]) ;
-    const [artist,setArtist] = useState<any[]>([]) ; 
+    const {data} = UseAllArtistQuery() ;  
 
     const handleQusetionBox = ()=>{
         setState(true) ;
     }
 
-    // const fetchPersonalityTypes = async ()=>{
-    //     const response = await AXIOS.get(API_URLS.GetPersonalityTypes) ; 
-    //     setTypes(response.data);
-    // }
-
-    const fetchArtists = async ()=>{
-      // const token = localStorage.getItem('access');
-      // console.log("token in home page: ",token)
-      // AXIOS.defaults.headers.common.Authorization = `Bearer ${token}`
-
-      const config = {
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
-        }
-      };
-
-        const response = await AXIOS.get(API_URLS.GetArtists,config);
-        setArtist(response.data);
-    }
-
-
-    useEffect(()=>{
-        // fetchPersonalityTypes();
-        fetchArtists() ;
-        console.log("user in home" , user)
-    },[])
-
-  
       const personalityTypes =  [
     {
       "title": "INFP",
@@ -127,26 +104,97 @@ export const HomePage : React.FC = () : JSX.Element =>{
         navigate(`/profile/${id}`)
     }
 
+    const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1
+  }
+};
+
+
+
+const Artist = data?.map((fame)=>{
+                          return <ArtistCard  charName={fame.name} charType={fame.type}
+                          career={fame.career}  charProfileUrl={fame.photoUrl} onclick={()=>{handleClick(fame.id)}}/>
+                        })
+
+
     return (
         <div className="homePage">
         
         <div className="content"> 
 
-
-            <div className="showTypes"> 
+        <div className="showFamePeople"> 
 
 
             <div className="titleContainer mb-10 w-[90%]">
-                 <div className="titleWrapper w-[40%]"> <h2 className='title'>تایپ های شخصیتی</h2> </div> </div>
+                 <div className="titleWrapper"> <h2 className='title'>افراد معروف</h2> </div> </div>
+            
+            <div className="famePeopleWrapper">
+
+                {/* <ul className="famePeopleList">
+                    
+                    {data?.map((fame)=>{
+
+                      if(counter <= 8 ){
+                        counter ++ ;
+                        return <ArtistCard  charName={fame.name} charType={fame.type}
+                        career={fame.career}  charProfileUrl={fame.photoUrl} onclick={()=>{handleClick(fame.id)}}/>
+                      }
+                        })}                  
+
+                </ul> */}
+
+
+                <Carousel  responsive={responsive}>{Artist}</Carousel>
+
+
+
+
+                <div className="moreButtonWrapper">
+                    <Link to={"/famouspeople"}> <MoreButton /></Link>
+                </div>
+
+            </div>
+        
+
+        
+        </div>
+
+
+
+        <div className="showTypes"> 
+
+
+            <div className="titleContainer mb-10 w-[90%]">
+              <div className="titleWrapper w-[40%]"> <h2 className='title'>تایپ های شخصیتی</h2> </div>    
+            </div>
             
             <div className="typesWrapper">
 
                 <ul className="typesList">
                     
                     {personalityTypes.map((type)=>{
-                        if(typesCounter <=6 ){
-                        return <PersonalityTypesCard typeTitle={type.title} />} 
-                        typesCounter ++ ;
+                        
+                        // if(typesCounter <=6 ){
+                        // typesCounter ++ ;
+                        // return <PersonalityTypesCard typeTitle={type.title} />} 
+
+                        return <PersonalityTypesCard typeTitle={type.title} />
+                        
                         })}
 
                 </ul>
@@ -162,40 +210,6 @@ export const HomePage : React.FC = () : JSX.Element =>{
         </div>
 
 
-
-
-        <div className="showFamePeople"> 
-
-
-            <div className="titleContainer mb-10 w-[90%]">
-                 <div className="titleWrapper"> <h2 className='title'>افراد معروف</h2> </div> </div>
-            
-            <div className="famePeopleWrapper">
-
-                <ul className="famePeopleList">
-                    
-                    {artist.map((fame)=>{
-                        if(counter <=8 ){
-                        
-                        counter ++ ;
-                        return <FamePeopleCard  charName={fame.name} charType={fame.type}
-                        career={fame.career}  charProfileUrl={fame.photoUrl} onclick={()=>{handleClick(fame.id)}}/>
-                        
-                        }})}
-
-                </ul>
-
-                <div className="moreButtonWrapper">
-                    <Link to={"/famouspeople"}> <MoreButton /></Link>
-                </div>
-
-            </div>
-        
-
-        
-        </div>
-        
-        
 
         <div className="qListWrappwe">
 
