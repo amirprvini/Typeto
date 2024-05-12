23ba750aa205e3a664eb5c524a408f5dec1f43d9
amirparvini <amirparvini2000@gmail.com>
2024-05-12 13:37:12 +0330

    convert css to tailwind for header's styles

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -27,20 +27,20 @@ const Header : React.FC<IHeaderProps>  = ({isAthenticated=false}) : JSX.Element 
     }
 
     return (
-        <div className="header">
-            <div className="topHeader ">
+        <div className="header w-full bg-black text-black flex flex-col justify-center items-center">
+            <div className="topHeader w-4/5 h-26 bg-black text-white flex justify-between items-center py-[.5rem] px-[1rem]">
 
-                    <div className="logo cursor-pointer" onClick={handleClick} >
-                        <div className="logoTitleWrapper">
-                            <h1 className="logoTitle">TYPETO.NET</h1>
-                            <h3 className="logoDescription">تایپتو انتخاب کن !</h3>
+                    <div className="logo cursor-pointer flex h-full" onClick={handleClick} >
+                        <div className="logoTitleWrapper w-full h-full flex flex-col items-start justify-start text-[2rem]">
+                            <h1 className="logoTitle font-serif">TYPETO.NET</h1>
+                            <h3 className="logoDescription text-white">تایپتو انتخاب کن !</h3>
                             </div>
                         <div className="logoIcon">
                         <GiBrain />
                         </div>
                     </div>
 
-                    {user?.isAuthenticated ?  <UserProfile/> : <div className="topHeaderButtonsWrapper">
+                    {user?.isAuthenticated ?  <UserProfile/> : <div className="topHeaderButtonsWrapper w-1/3 flex items-center justify-center space-x-4 space-y-4">
                         <Link to="/signUp"> <TopHeaderButton title="ثبت نام | ورود" icon={<CgLogIn />}/> </Link>
                         {/* <Link to="/login"> <TopHeaderButton title="وارد شوید" /> </Link> */}
                     </div>}
@@ -48,7 +48,7 @@ const Header : React.FC<IHeaderProps>  = ({isAthenticated=false}) : JSX.Element 
 
                 </div>
 
-                <div className="navBarWrapper">
+                <div className="navBarWrapper w-full h-24 flex justify-center items-center ">
                     <NavBar/>
                 </div>
         </div>
