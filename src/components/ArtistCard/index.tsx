2a8f3a7984235eefe307c4949e9dce1bcaf74b0f
amirparvini <amirparvini2000@gmail.com>
2024-06-28 01:23:15 +0330

    artist card updated

--- src/components/ArtistCard/index.tsx
+++ src/components/ArtistCard/index.tsx
@@ -17,28 +17,33 @@ const ArtistCard : React.FC<IArtistCard> = ({charName,charType,career,charProfil
     
     
     return ( 
-        <div className="ArtistCard cursor-pointer" onClick={onclick}>
+        <div className="ArtistCard cursor-pointer bg-black flex w-[98%]
+        text-[1.5rem] flex-col justify-around h-85 rounded-2xl sm:w-[99%] md:w-[90%] lg:w-[85%]" onClick={onclick}>
 
+            <div className='ArtistCardHeader h-5/6 w-full flex flex-col rounded-t-[.9rem] justify-start'>
 
-            <div className="charTypeWrapper">
-                <div className="charType"> <h3 className="charTypeTitle">{charType}</h3> </div>
-            </div>
+                <div className="profileWrapper w-full flex justify-center h-3/6">
 
-            <div className='ArtistCardHeader'>
-
-            <div className="charInfoWrapper">
-                
-                <div className="charInfo">
-                <h2 className="charName">{charName}</h2>
-                <p className="career"> {career} </p> 
+                    <div className="profile relative py-8 px-3 w-[80%] h-[150px] flex justify-center items-center"> 
+                        <img src= {charProfileUrl} alt={charName + "photo"}
+                        className='image absolute top-2/4 left-2/4 w-full h-auto'></img> 
+                    </div>
                 </div>
 
+                <div className="charInfoWrapper w-full flex justify-center h-3/6 items-start">
+                    <div className="charInfo px-2 w-full h-[8rem] flex flex-col justify-start items-start">
+                        <h2 className="charName flex justify-start w-full">{charName}</h2>
+                        <p className="career text-gray-400 text-base justify-self-center"> {career} </p> 
+                    </div>
+                </div>
+        
             </div>
 
-             <div className="profileWrapper">
-                <div className="profile"> <img src= {charProfileUrl} alt={charName + "photo"}></img> </div>
-            </div>
-        
+
+             <div className="charTypeWrapper w-full flex justify-center h-1/6">
+                    <div className="charType flex justify-center 
+                    w-full h-full items-end rounded-b-[.9rem] text-3xl rounded-tr-none"> <h3 className="charTypeTitle text-goldenrod uppercase">{charType}</h3> 
+                    </div>
             </div>
 
         </div>
