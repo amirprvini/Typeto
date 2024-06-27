d4ad33c431de45271f8e901fd0882a468540ac13
amirparvini <amirparvini2000@gmail.com>
2024-06-28 01:25:15 +0330

    MBTI drop down created

--- src/components/MBTIDropDown/index.tsx
+++ src/components/MBTIDropDown/index.tsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.css'
 import PersonalityTypesCard from '../PersonalityTypesCard';
 import MBTIButton from '../Buttons/MBTIButton';
+import { IoIosArrowDown } from "react-icons/io";
 
 
 interface DropDownPros {
     onCompleteDropDown : (mbtiType:string)=> void ; 
+    widthProp?: string;
+    bgProp ?: string ; 
+    justifyProp ?: string ; 
 }
-const DropDown : React.FC<DropDownPros> = ({onCompleteDropDown}) :JSX.Element => {
+const DropDown : React.FC<DropDownPros> = ({onCompleteDropDown,widthProp,bgProp,justifyProp}) :JSX.Element => {
 
+
+  const [state,setState] = useState<string>('INFP');
     const personalityTypes =  [
     {
       "title": "INFP",
@@ -76,27 +82,52 @@ const DropDown : React.FC<DropDownPros> = ({onCompleteDropDown}) :JSX.Element =>
     }
   ] ;
 
+
+  const handleClick = ()=>{
+    const dropDown = document.querySelector('.dropDowm') ;
+    const arrowWrapper = document.querySelector('.arrowWrapper') ;
+    dropDown?.classList.toggle('hidden') ;  
+    arrowWrapper?.classList.toggle("active") ; 
+  }
+
+  const handleSelect = (type:string)=>{
+    
+    const dropDown = document.querySelector('.dropDowm') ;
+    const arrowWrapper = document.querySelector('.arrowWrapper') ;
+    dropDown?.classList.toggle('hidden') ;  
+    arrowWrapper?.classList.toggle("active") ;  
+
+    setState(type) ; 
+  }
+
   return (
-    <div className='dropDownWrapper'>
+    <div className={`dropDownWrapper flex-col mb-4 w-${widthProp}`}>
 
-        <div className="dropDownContainer">
+        <div className={`tContainer w-full flex justify-${justifyProp} mb-4`}>
+            <p className="ttitle text-[1rem]">تایپ شخصیتی خود را انتخاب کنید</p>
+        </div>
 
-            <div className="dropDownTitle px-8">
-             *تایپ شخصیتی خود را انتخاب کنید
+        <div className="dropDownContainer w-full items-center justify-start">
+
+            <div className={`dropDownTitleWrapper bg-${bgProp} flex space-x-2 w-full px-4 py-2 border-gray border-solid border-[1px] rounded-xl cursor-pointer`} onClick={handleClick} >
+                <div className="arrowWrapper transition duration-500 flex justify-center items-center ml-1"><IoIosArrowDown /></div>
+                <p className="dropDownTitle">{state}</p>
             </div>
       
-        <div className="dropDowm">
+            <div className="dropDowm  mb-4 w-full border-gray border-solid border-[1px] rounded-xl px-4 py-2 max-h-44 overflow-y-scroll hidden">
 
-            <ul className="typesList">
+                <ul className="typesList flex flex-col flex-wrap space-x-2 space-y-2 justify-center items-start ">
 
-                { personalityTypes.map((type)=>{
-                    return <li className='w-[12%] flex justify-center' key={type.id}> <MBTIButton typeTitle={type.title} onCompleteButton={(type)=>{
-                      onCompleteDropDown(type)
-                    }}/> </li>
-            })}
+                    { personalityTypes.map((type)=>{
+                        return <li className='w-full' key={type.id}> <MBTIButton typeTitle={type.title} onCompleteButton={(type)=>{
+                          onCompleteDropDown(type)
+                          console.log('clicked !!!',type) ;
+                          handleSelect(type) ; 
+                        }}/> </li>
+                })}
 
-            </ul>
-        </div>
+                </ul>
+            </div>
 
         </div>
 
