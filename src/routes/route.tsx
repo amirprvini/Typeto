9906e0a257b68c0eea1ecc04f3cbf8a063b150ae
amirparvini <amirparvini2000@gmail.com>
2024-08-09 10:27:41 +0330

    routes updated

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {lazy,Suspense} from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Layout from '../components/Layout'
 import { HomePage } from '../pages/HomePage'
@@ -12,9 +12,14 @@ import { SignUpPage } from '../pages/SignUpPage'
 import TypesInfoPage from '../pages/TypesInfoPage'
 import { UserProfile } from '../components/Buttons/UserProfile'
 import UserProfilePage from '../pages/UserProfilePage'
+import EditUserProfilePage from '../pages/EditUserProfilePage'
+import SignOut from '../components/SignOut'
 
 interface IRouteProvider {}
-export const RouteProvider : React.FC = () :JSX.Element => {
+
+// const SignUpPage = lazy(()=>import('/signUp'));
+
+export const RouteProvider : React.FC<IRouteProvider> = () :JSX.Element => {
   return (
 
     <BrowserRouter>
@@ -31,46 +36,16 @@ export const RouteProvider : React.FC = () :JSX.Element => {
             <Route path='/memes' element={<HomePage/>}/>
             <Route path='/contact' element={<ContactPage/>}/>
 
-            {/* <Route path={'/profile/:id'} element={<ProfilePage/>}/> */}
-            <Route path={'/profile/:id'} element={<ProfilePage/>}/>
+            <Route path='/profile/:id' element={<ProfilePage/>}/>
 
-            <Route path='/userProfile' element={<UserProfilePage/>} />
-
-
-            <Route path='/personalitytypes/INFJ' element={<TypesInfoPage title='INFG'/>}/>
-            <Route path='/personalitytypes/INFP' element={<TypesInfoPage title='INFP'/>}/>
-            <Route path='/personalitytypes/INTP' element={<TypesInfoPage title='INTP'/>}/>
-            <Route path='/personalitytypes/INTJ' element={<TypesInfoPage title='INTJ'/>}/>
-            <Route path='/personalitytypes/ENTP' element={<TypesInfoPage title='ENTP'/>}/>
-            <Route path='/personalitytypes/ENFP' element={<TypesInfoPage title='ENFP'/>}/>
-            <Route path='/personalitytypes/ISTP' element={<TypesInfoPage title='ISTP'/>}/>
-            <Route path='/personalitytypes/ISFP' element={<TypesInfoPage title='ISFP'/>}/>
-            <Route path='/personalitytypes/ENTJ' element={<TypesInfoPage title='ENTJ'/>}/>
-            <Route path='/personalitytypes/ENFJ' element={<TypesInfoPage title='ENFJ'/>}/>
-            <Route path='/personalitytypes/ISTJ' element={<TypesInfoPage title='ISTJ'/>}/>
-            <Route path='/personalitytypes/ESTP' element={<TypesInfoPage title='ESTP'/>}/>
-            <Route path='/personalitytypes/ESTP' element={<TypesInfoPage title='ESTP'/>}/>
-            <Route path='/personalitytypes/ESFP' element={<TypesInfoPage title='ESFP'/>}/>
-            <Route path='/personalitytypes/ESEI' element={<TypesInfoPage title='ESEI'/>}/>
-            <Route path='/personalitytypes/ESTI' element={<TypesInfoPage title='ESTI'/>}/>
-
-            <Route path='/famepeopletypes/INFG' element={<FamePeopleWithSpecialTypes title='INFG' />}/>
-            <Route path='/famepeopletypes/INFP' element={<FamePeopleWithSpecialTypes title='INFP' />}/>
-            <Route path='/famepeopletypes/INTP' element={<FamePeopleWithSpecialTypes title='INTP' />}/>
-            <Route path='/famepeopletypes/INTJ' element={<FamePeopleWithSpecialTypes title='INTJ' />}/>
-            <Route path='/famepeopletypes/ENTP' element={<FamePeopleWithSpecialTypes title='ENTP' />}/>
-            <Route path='/famepeopletypes/ENFP' element={<FamePeopleWithSpecialTypes title='ENFP' />}/>
-            <Route path='/famepeopletypes/ISTP' element={<FamePeopleWithSpecialTypes title='ISTP' />}/>
-            <Route path='/famepeopletypes/ISFP' element={<FamePeopleWithSpecialTypes title='ISFP' />}/>
-            <Route path='/famepeopletypes/ENTJ' element={<FamePeopleWithSpecialTypes title='ENTJ' />}/>
-            <Route path='/famepeopletypes/ENFJ' element={<FamePeopleWithSpecialTypes title='ENFJ' />}/>
-            <Route path='/famepeopletypes/ISTJ' element={<FamePeopleWithSpecialTypes title='ISTJ' />}/>
-            <Route path='/famepeopletypes/ESTP' element={<FamePeopleWithSpecialTypes title='ESTP' />}/>
-            <Route path='/famepeopletypes/ESTP' element={<FamePeopleWithSpecialTypes title='ESTP' />}/>
-            <Route path='/famepeopletypes/ESFP' element={<FamePeopleWithSpecialTypes title='ESFP' />}/>
-            <Route path='/famepeopletypes/ESEI' element={<FamePeopleWithSpecialTypes title='ESEI' />}/>
-            <Route path='/famepeopletypes/ESTI' element={<FamePeopleWithSpecialTypes title='ESTI' />}/>
-
+            <Route path='/userProfile/' element={<UserProfilePage/>}/>
+            <Route path='/userProfile/editProfile' element={<EditUserProfilePage/>} />
+            <Route path='/userProfile/signOut' element={<SignOut/>} />
+            
+            <Route path='/personalitytypes/:type' element={<TypesInfoPage />}/>
+            
+            <Route path='/famepeopletypes/:type' element={<FamePeopleWithSpecialTypes />}/>
+            
           </Route>
 
       </Routes>
