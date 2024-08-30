d6fa8b1c27ec0b09c824a63ec2cded4066bcfed4
amirparvini <amirparvini2000@gmail.com>
2024-08-30 12:59:51 +0330

    renamed this page

--- src/pages/LogOutPage/index.tsx
+++ src/pages/LogOutPage/index.tsx
@@ -4,7 +4,7 @@ import LogOut from '../../components/LogOut'
 
 interface LogOutPageProps {}
 
-const SignOutPage : React.FC<LogOutPageProps> = ():JSX.Element => {
+const LogOutPage : React.FC <LogOutPageProps> = ():JSX.Element => {
   return <div className="userProfilePage w-[90%] sm:w-3/4 lg:w-2/5 bg-black text-white text-7xl flex justify-center"> 
     
         <div className="signOutPageWrapper w-3/4 my-4 "> <LogOut/> </div>
@@ -12,4 +12,4 @@ const SignOutPage : React.FC<LogOutPageProps> = ():JSX.Element => {
     </div>
 }
 
-export default SignOutPage
+export default LogOutPage
